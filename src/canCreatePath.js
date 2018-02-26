6c5f3f16a42b1d597f6647a8afc26a9a5dd55810
Thijs <thijs@mediamonks.com>
2018-02-26 15:36:45 +0100

    move createPath and canCreatePath to separate files

---
+++ src/canCreatePath.js
@@ -0,0 +1,15 @@
+/**
+ * Checks if a path can be created from a template with the given params.
+ *
+ * @param template The configured path template (e.g. /foo/:bar or /user(/:id))
+ * @param params The param values that is used to replace the params in the template (e.g. { bar: 'baz' }
+ * @returns {boolean} return true if a path can be created, otherwise false
+ */
+export default function canCreatePath(template, params = {}) {
+	try {
+		createPath(path, params);
+		return true;
+	} catch (error) {
+		return false;
+	}
+}
---
+++ src/createPath.js
@@ -0,0 +1,47 @@
+/**
+ * Builds a path from a template and replaces the params.
+ *
+ * It will throw an error is mandatory params are not available in the params object.
+ *
+ * @param template The configured path template (e.g. /foo/:bar or /user(/:id))
+ * @param params The param values that is used to replace the params in the template (e.g. { bar: 'baz' }
+ */
+export default function createPath (template, params = {}) {
+	// Match text within parenthesis that are no variables (does not start with ":")
+	const PARENTHESIZED_CONST = /\(([^:\()]+?)\)/g;
+
+	// temporary replace values for brackets
+	const OPEN_BRACKET = '#OPEN-BRACKET#';
+	const CLOSE_BRACKET = '#CLOSE-BRACKET#';
+
+	return template
+		// first replace all params
+		.replace(/:(\w+)/g, (match, param) =>
+			(typeof params[param] !== 'undefined' ?
+				`${params[param]}`
+					// temporary replace brackets to avoid conflicts
+					.replace(/\(/g, OPEN_BRACKET)
+					.replace(/\)/g, CLOSE_BRACKET)
+				:
+				match))
+
+		// remove parenthesis for resolved optional parts
+		.replace(/.+/g, (temp) => {
+			while (PARENTHESIZED_CONST.test(temp)) {
+				temp = temp.replace(PARENTHESIZED_CONST, (match, part) => part);
+			}
+			return temp;
+		})
+
+		// remove the other (unresolved) optional parts
+		.replace(/\(.+?\)/g, () => '')
+
+		// do we still have params left?
+		.replace(/:(\w+)/g, (match, param) => {
+			throw new Error(`Param "${param}" is missing in params (${JSON.stringify(params)}), needed for '${path}'`);
+		})
+
+		// replace temporary brackets with real brackets
+		.replace(new RegExp(OPEN_BRACKET, 'g'), '(')
+		.replace(new RegExp(CLOSE_BRACKET, 'g'), ')');
+}
