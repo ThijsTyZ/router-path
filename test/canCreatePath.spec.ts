96a03648c49afed7adb75b369a0cd4d08bdbf224
Thijs <thijs@tyz.nl>
2023-10-09 16:24:06 +0200

    replace `equal` with `strictEqual` in the unit tests

--- test/canCreatePath.spec.ts
+++ test/canCreatePath.spec.ts
@@ -4,33 +4,33 @@ const assert = require('assert');
 
 describe('canCreatePath', () => {
 	it('should return true', () => {
-		assert.equal(canCreatePath('user/'), true);
-		assert.equal(canCreatePath('user/', { id: 1 }), true);
-		assert.equal(canCreatePath('/user'), true);
-		assert.equal(canCreatePath('/user/'), true);
-		assert.equal(canCreatePath('user/:id', { id: 1 }), true);
-		assert.equal(canCreatePath('user/:id/:slug', { id: 1, slug: 'john-do' }), true);
-		assert.equal(canCreatePath('user/:id(/:slug)', { id: 1, slug: 'john-do' }), true);
-		assert.equal(canCreatePath('user/:id(/:slug)/', { id: 1, slug: 'john-do' }), true);
-		assert.equal(canCreatePath('user/:id/(:slug)', { id: 1, slug: 'john-do' }), true);
-		assert.equal(canCreatePath('user/:id(/:slug)', { id: 1 }), true);
-		assert.equal(canCreatePath('user/:id/(:slug)', { id: 1 }), true);
-		assert.equal(canCreatePath('/:id/:slug/:state', { id: 1, slug: 'john', state: 'x' }), true);
-		assert.equal(canCreatePath(':id/:slug/:state', { id: 1, slug: 'john', state: 'x' }), true);
-		assert.equal(canCreatePath('user/:id/(:slug/:foo)', { id: 1 }), true);
-		assert.equal(canCreatePath('user/:id/(:slug/:foo)', { id: 1, slug: 'slug', foo: 'bar' }), true);
-		assert.equal(canCreatePath('user/:id/(:slug/:foo)', { id: 1, foo: 'bar' }), true);
-		assert.equal(canCreatePath('user/:id/(:slug)/(:foo)', { id: 1, foo: 'bar' }), true);
-		assert.equal(canCreatePath('(:slug/:foo)'), true);
-		assert.equal(canCreatePath('/(:slug/:foo)'), true);
-		assert.equal(canCreatePath('user/:id/(:slug/x/:foo)', { id: 1, slug: 'a', foo: 'bar' }), true);
-		assert.equal(canCreatePath('user/:id/(:slug/x/:foo)', { id: 1, slug: 'a' }), true);
-		assert.equal(canCreatePath('user/:id/', { id: '(id)' }), true);
-		assert.equal(canCreatePath('user/:id/:slug', { id: '(id)', slug: ('(slug)') }), true);
-		assert.equal(canCreatePath('/(shop(/:category))', { category: 'prints' }), true);
+		assert.strictEqual(canCreatePath('user/'), true);
+		assert.strictEqual(canCreatePath('user/', { id: 1 }), true);
+		assert.strictEqual(canCreatePath('/user'), true);
+		assert.strictEqual(canCreatePath('/user/'), true);
+		assert.strictEqual(canCreatePath('user/:id', { id: 1 }), true);
+		assert.strictEqual(canCreatePath('user/:id/:slug', { id: 1, slug: 'john-do' }), true);
+		assert.strictEqual(canCreatePath('user/:id(/:slug)', { id: 1, slug: 'john-do' }), true);
+		assert.strictEqual(canCreatePath('user/:id(/:slug)/', { id: 1, slug: 'john-do' }), true);
+		assert.strictEqual(canCreatePath('user/:id/(:slug)', { id: 1, slug: 'john-do' }), true);
+		assert.strictEqual(canCreatePath('user/:id(/:slug)', { id: 1 }), true);
+		assert.strictEqual(canCreatePath('user/:id/(:slug)', { id: 1 }), true);
+		assert.strictEqual(canCreatePath('/:id/:slug/:state', { id: 1, slug: 'john', state: 'x' }), true);
+		assert.strictEqual(canCreatePath(':id/:slug/:state', { id: 1, slug: 'john', state: 'x' }), true);
+		assert.strictEqual(canCreatePath('user/:id/(:slug/:foo)', { id: 1 }), true);
+		assert.strictEqual(canCreatePath('user/:id/(:slug/:foo)', { id: 1, slug: 'slug', foo: 'bar' }), true);
+		assert.strictEqual(canCreatePath('user/:id/(:slug/:foo)', { id: 1, foo: 'bar' }), true);
+		assert.strictEqual(canCreatePath('user/:id/(:slug)/(:foo)', { id: 1, foo: 'bar' }), true);
+		assert.strictEqual(canCreatePath('(:slug/:foo)'), true);
+		assert.strictEqual(canCreatePath('/(:slug/:foo)'), true);
+		assert.strictEqual(canCreatePath('user/:id/(:slug/x/:foo)', { id: 1, slug: 'a', foo: 'bar' }), true);
+		assert.strictEqual(canCreatePath('user/:id/(:slug/x/:foo)', { id: 1, slug: 'a' }), true);
+		assert.strictEqual(canCreatePath('user/:id/', { id: '(id)' }), true);
+		assert.strictEqual(canCreatePath('user/:id/:slug', { id: '(id)', slug: ('(slug)') }), true);
+		assert.strictEqual(canCreatePath('/(shop(/:category))', { category: 'prints' }), true);
 	});
 
 	it('should return false', () => {
-		assert.equal(canCreatePath('user/:id/'), false);
+		assert.strictEqual(canCreatePath('user/:id/'), false);
 	});
 });
